54cb08e94d18de5b214a76a8feba08cc60da41bf
Shyam9505 <147794279+Shyam9505@users.noreply.github.com>
2023-10-13 16:19:35 +0530

    Add files via upload

---
+++ string_reverse.py
@@ -0,0 +1,10 @@
+name = "Shyam Raj"
+print(name[1:5])
+name = ("samrat," "shyam")
+print(name[-1::-2])
+attendees : list=["battu", "samrat", "raj",]
+print (attendees[-1])
+print (attendees[-2])
+print (attendees[-3])
+name = "shyam"[: :-1]
+print(name[-5: ])
